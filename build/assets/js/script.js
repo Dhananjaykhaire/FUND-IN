1733614b9be975b551f154bb7f2a2738a4c9d300
Dhananjay Khaire <136916045+MRDKFucker@users.noreply.github.com>
2024-11-13 14:57:04 +0530

    Updated GitHub Actions workflow for Firebase deployment

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -3,5 +3,27 @@
 
 
 /**
- * navbar
+ * navbar toggle
  */
+
+const navOpenBtn = document.querySelector("[data-nav-Open-btn]");
+const navbar = document.querySelector ("[data-navbar]");
+const navClosebtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navClosebtn];
+
+for (let 1 = 0; i < navElemArr.length; i++){
+    navElemArr[i].addEventListener("click", function()){
+        navbar.classList.toggle("active");
+    });
+}
+/**
+ * toggle navbar when click any navbar link
+ */
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i< navbarLinks.length; i++){
+    navbarLinks[i].addEventListener("click", function (){
+        navbar.classList.remove("active");
+    });
+}
