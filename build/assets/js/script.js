357f7a6a90b0d89373aec91583d55d2be1624634
Dhananjay Khaire <136916045+MRDKFucker@users.noreply.github.com>
2024-11-20 00:00:21 +0530

    Updated GitHub Actions workflow for Firebase deployment

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -3,36 +3,44 @@
 
 
 /**
- * Navbar toggle
+ * navbar toggle
  */
 
-const navOpenbtn = document.querySelector("[data-nav-Open-btn]");
-const navbar = document.querySelector ("[data-navbar]");
-const navClosebtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-const navElemArr = [navOpenbtn, navClosebtn];
+const navElemArr = [navOpenBtn, navCloseBtn];
 
-for (let i = 0; i < navElemArr.length; i++){
-    navElemArr[i].addEventListener("click", function() {
-        navbar.classList.toggle("active");
-    });
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 }
+
 /**
- * Toggle navbar when click any navbar link
+ * toggle navbar when click any navbar link
  */
+
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-for (let i = 0; i< navbarLinks.length; i++){
-    navbarLinks[i].addEventListener("click", function (){
-        navbar.classList.remove("active");
-    });
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
 }
- /**
-  * Header active when window scroll down
-  */
- const header = document.querySelector("[data-header]");
 
- window.addEventListener("scroll", function(){
-   window.scrollY >= 50 ? header.classList.add("active")
+
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50 ? header.classList.add("active")
     : header.classList.remove("active");
- });
+});
