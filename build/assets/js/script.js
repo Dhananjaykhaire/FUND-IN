a77d3c0fcc5d46eab332fa47ed79b972c60385b2
Dhananjay Khaire <136916045+MRDKFucker@users.noreply.github.com>
2024-11-20 12:59:52 +0530

    Updated GitHub Actions workflow for Firebase deployment

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -45,44 +45,8 @@ window.addEventListener("scroll", function () {
     : header.classList.remove("active");
 });
 
-// Modal and Form Elements
-const authModal = document.getElementById("authModal");
-const authToggleBtn = document.getElementById("authToggleBtn");
-const closeModal = document.getElementById("closeModal");
-const signInForm = document.getElementById("signInForm");
-const signUpForm = document.getElementById("signUpForm");
-const showSignUp = document.getElementById("showSignUp");
-const showSignIn = document.getElementById("showSignIn");
 
-// Show Modal with Sign In Form
-authToggleBtn.addEventListener("click", () => {
-  authModal.style.display = "block";
-  signInForm.classList.add("active");
-  signUpForm.classList.remove("active");
-});
+  document.getElementById("signInButton").addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
 
-// Close Modal
-closeModal.addEventListener("click", () => {
-  authModal.style.display = "none";
-});
-
-// Switch to Sign Up Form
-showSignUp.addEventListener("click", (e) => {
-  e.preventDefault();
-  signInForm.classList.remove("active");
-  signUpForm.classList.add("active");
-});
-
-// Switch to Sign In Form
-showSignIn.addEventListener("click", (e) => {
-  e.preventDefault();
-  signUpForm.classList.remove("active");
-  signInForm.classList.add("active");
-});
-
-// Close Modal on Outside Click
-window.addEventListener("click", (event) => {
-  if (event.target === authModal) {
-    authModal.style.display = "none";
-  }
-});
